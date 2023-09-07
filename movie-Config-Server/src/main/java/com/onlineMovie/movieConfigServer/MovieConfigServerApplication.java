d0f87b5e77b0f2d74d2808150b8a90ae9a20e819
Navya <navyanambiar99@gmail.com>
2023-09-07 13:50:21 +0530

    added

--- movie-Config-Server/src/main/java/com/onlineMovie/movieConfigServer/MovieConfigServerApplication.java
+++ movie-Config-Server/src/main/java/com/onlineMovie/movieConfigServer/MovieConfigServerApplication.java
@@ -2,7 +2,8 @@ package com.onlineMovie.movieConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class MovieConfigServerApplication {
 
