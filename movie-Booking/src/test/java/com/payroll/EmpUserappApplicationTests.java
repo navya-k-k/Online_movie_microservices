c9f2a4cddb1a4d76f9caca926a888bdac96438ad
NIKITA6233 <nikitagaikwad1575@gmail.com>
2023-09-09 13:52:13 +0530

    booking

---
+++ movie-Booking/src/test/java/com/payroll/EmpUserappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpUserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
